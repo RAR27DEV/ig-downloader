19e936f84831f0513c6fe693602088247082755d
unknown <rafiqanderama1@gmail.com>
2025-12-02 22:54:12 +0700

    ready for railway deploy

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const cors = require("cors");
 const { execFile } = require("child_process");
 
 const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.use(cors());
 app.use(express.json());
 
